fabddb594a1091433a8a66f6f9d2ac148302d00a
lukedee-b <n00220386@iadt.ie>
2023-11-27 11:03:49 +0000

    Created user roles relationship

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use App\Models\Role;
 
 class UserSeeder extends Seeder
 {
@@ -13,11 +14,27 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        $role_admin = Role::where('name', 'admin')->first();
+
+        $role_user = Role::where('name', 'user')->first();
+
+
+
         $admin = new User;
         $admin->name = "Luke Dee";
-        $admin->email = "n00220386@iadt.ie";
+        $admin->email = "admin@example.com";
         $admin->password = "secret123";
         $admin->save();
 
+        //attach admin role to the user created above
+        $admin->roles()->attach($role_admin);
+
+        $user = new User;
+        $user->name = "Ben Kershaw";
+        $user->email = "user@example.com";
+        $user->password = "secret1234";
+        $user->save();
+
+        $user->roles()->attach($role_user);
     }
 }
